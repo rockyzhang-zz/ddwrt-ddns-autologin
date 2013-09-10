40289f79cf342dbff546809fc5b603ac0c954f43
Rocky Zhang <zhangyan.hit@gmail.com>
2013-09-10 21:08:43 +0800

    fix spacing
    Signed-off-by: Rocky Zhang <zhangyan.hit@gmail.com>

--- ddwrt-dyndns-autologin.pl
+++ ddwrt-dyndns-autologin.pl
@@ -8,7 +8,9 @@ my $url  = "https://account.dyn.com";
 my $USERNAME = `nvram get ddns_username_buf`;
 my $PASSWORD = `nvram get ddns_password_buf`;
 
-$mech->add_header( User-agent => 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36');
+$mech->add_header( User-agent => "Mozilla/5.0 (Windows NT 6.1; WOW64) \
+                                  AppleWebKit/537.36 (KHTML, like Gecko) \
+                                  Chrome/27.0.1453.110 Safari/537.36" );
 $mech->get($url);
 
 $result = $mech->submit_form(
