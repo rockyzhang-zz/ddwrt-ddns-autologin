626223ae74b8426b810126a1db82589cbc6c4cc3
Rocky Zhang <zhangyan.hit@gmail.com>
2013-10-16 22:06:48 +0800

    change password_buf to passwd_buf
    Signed-off-by: Rocky Zhang <zhangyan.hit@gmail.com>

--- ddwrt-dyndns-autologin.pl
+++ ddwrt-dyndns-autologin.pl
@@ -7,7 +7,7 @@ my $mech = WWW::Mechanize->new();
 my $url  = "https://account.dyn.com";
 
 my $USERNAME = `nvram get ddns_username_buf`;
-my $PASSWORD = `nvram get ddns_password_buf`;
+my $PASSWORD = `nvram get ddns_passwd_buf`;
 
 $mech->add_header( "User-agent" => "Mozilla/5.0 (Windows NT 6.1; WOW64) \
                                   AppleWebKit/537.36 (KHTML, like Gecko) \
