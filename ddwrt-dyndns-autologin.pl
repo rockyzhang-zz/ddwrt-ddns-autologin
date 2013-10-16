0b2c09dd8178f7777c109e3220b0848c825a1b64
Rocky Zhang <zhangyan.hit@gmail.com>
2013-10-16 21:57:53 +0800

    change form number
    Signed-off-by: Rocky Zhang <zhangyan.hit@gmail.com>

--- ddwrt-dyndns-autologin.pl
+++ ddwrt-dyndns-autologin.pl
@@ -2,19 +2,22 @@
 
 use WWW::Mechanize;
 
+
 my $mech = WWW::Mechanize->new();
 my $url  = "https://account.dyn.com";
 
 my $USERNAME = `nvram get ddns_username_buf`;
 my $PASSWORD = `nvram get ddns_password_buf`;
 
-$mech->add_header( User-agent => "Mozilla/5.0 (Windows NT 6.1; WOW64) \
+$mech->add_header( "User-agent" => "Mozilla/5.0 (Windows NT 6.1; WOW64) \
                                   AppleWebKit/537.36 (KHTML, like Gecko) \
                                   Chrome/27.0.1453.110 Safari/537.36" );
 $mech->get($url);
 
+
+
 $result = $mech->submit_form(
-    form_number => 1,
+    form_number => 2,
     fields      => {
         username => $USERNAME,
         password => $PASSWORD,
