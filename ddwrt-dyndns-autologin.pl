cb2c4803061eadb73e3d81660a8d55f7d64adc35
Rocky Zhang <zhangyan.hit@gmail.com>
2013-10-16 23:15:02 +0800

    adding trim to the strings
    Signed-off-by: Rocky Zhang <zhangyan.hit@gmail.com>

--- ddwrt-dyndns-autologin.pl
+++ ddwrt-dyndns-autologin.pl
@@ -9,6 +9,10 @@ my $url  = "https://account.dyn.com";
 my $USERNAME = `nvram get ddns_username_buf`;
 my $PASSWORD = `nvram get ddns_passwd_buf`;
 
+#Trim the strings
+$USERNAME  =~ s/^\s+|\s+$//g;
+$PASSWORD  =~ s/^\s+|\s+$//g;
+
 $mech->add_header( "User-agent" => "Mozilla/5.0 (Windows NT 6.1; WOW64) \
                                   AppleWebKit/537.36 (KHTML, like Gecko) \
                                   Chrome/27.0.1453.110 Safari/537.36" );
